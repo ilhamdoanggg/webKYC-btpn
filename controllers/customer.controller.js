e2a239f7c930d8756cd5a0147d883ed3c12a0908
aloelsafzi <samsulfauzialoel@gmail.com>
2020-06-14 14:49:31 +0700

    change customer controller

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,5 +1,4 @@
 const customerService = require('../services/customer.service');
-const Customer = require('../models').customer;
 
 module.exports = (app) => {
     app.post('/create-customer', (req, res) => {
@@ -10,9 +9,15 @@ module.exports = (app) => {
             result: req.body.result,
             note: req.body.note
         }
-        let result = customerService.create(customer);
-        console.log("resultnya : ", result);
-        return res.redirect('back')
+
+        customerService.create(customer)
+            .then(result => {
+                const { message, isSuccess } = result;
+                if (isSuccess) req.flash('messageSuccess', message);
+                else req.flash('messageErorr', message);
+            })
+            .catch(err => { console.error(err) })
+            .finally(() => { return res.redirect('back'); })
     });
 
     app.post('/update-customer', (req, res) => {
@@ -24,41 +29,27 @@ module.exports = (app) => {
             result: req.body.result,
             note: req.body.note
         }
-        customerService.update(customer);
-        return res.redirect('/sales/offering-data-potensi')
-    });
-
-    app.get('/sales/offering-data-potensi', (req, res) => {
-        Customer.findAll().then(customers => {
-            res.render('pages/offering-data-potensi', { user: req.user, customers, pageTitle: 'Offering - Data Potensi' });
-        }).catch(function (err) {
-            return null;
-        })
+        customerService.update(customer)
+            .then(result => {
+                const { message, isSuccess } = result;
+                if (isSuccess) req.flash('messageSuccess', message);
+                else req.flash('messageErorr', message);
+            })
+            .catch(err => { console.error(err) })
+            .finally(() => { return res.redirect('/sales/offering-data-potensi') })
     });
 
     app.get('/delete-customer/:id', (req, res) => {
         const id = req.params.id;
-        customerService.delete(id);
-        return res.redirect('back')
+
+        customerService.delete(id)
+            .then(result => {
+                const { message, isSuccess } = result;
+                if (isSuccess) req.flash('messageSuccess', message);
+                else req.flash('messageErorr', message);
+            })
+            .catch(err => console.error(err))
+            .finally(() => { return res.redirect('back') })
     });
-
-    app.get('/sales/offering-canvas', (req, res) => {
-        Customer.findById(req.query.id).then(customer => {
-            res.render('pages/offering-canvas', { user: req.user, customer, pageTitle: 'Offering - Canvasing' });
-        }).catch(function (err) {
-            return null;
-        })
-    });
-
-
-    // route confirmations
-    app.get('/sales/confirmation', (req, res) => {
-        Customer.findAll().then(customers => {
-            res.render('pages/confirmation', { user: req.user, customers, pageTitle: 'Confirmation' });
-        }).catch(function (err) {
-            return null;
-        })
-    });
-
 
 }
